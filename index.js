89e8ef3569be7712eb838299a0b590af9649cf21
Branden Mosley <branden.mosley1337@gmail.com>
2018-01-22 22:38:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemPrice = Math.floor(Math.random() * 100)
+ var object = {[item]: itemPrice}
+ cart.push(object)
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 1) {
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at \$${Object.values(cart[0])}.`)
+  } else if (cart.length === 2) {
+    var newArray = []
+    for (var i = 0; i < cart.length; i++) {
+      newArray.push(`${Object.keys(cart[i])} at \$${Object.values(cart[i])}`)
+      } console.log(`In your cart, you have ${newArray.join(", and ")}.`)
+  
+  } else if (cart.length >= 3) {
+    
+  } else {
+    console.log('Your shopping cart is empty.')
+    return 'Your shopping cart is empty.'
+  }
 }
 
 function total() {
